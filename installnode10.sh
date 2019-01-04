f1aa67ccb5a136f72a4a42f65f054f19dd08ee26
BIRD-MAC <chalermporn@aspiron.co.th>
2019-01-04 11:29:58 +0700

    init

---
+++ installnode10.sh
@@ -0,0 +1,6 @@
+
+brew search node;
+brew unlink node;
+brew install node@10;
+brew link node@10 --force;
+# https://medium.com/@katopz/how-to-install-specific-nodejs-version-c6e1cec8aa11
---
+++ installnode11.sh
@@ -0,0 +1,6 @@
+
+brew search node;
+brew unlink node;
+brew install node;
+brew link node --force;
+# https://medium.com/@katopz/how-to-install-specific-nodejs-version-c6e1cec8aa11
